172cc2020709440fca9568314b168e30a4b02e2e
Chika Stephen <c4chika.stephen@gmail.com>
2022-03-25 14:08:30 +0000

    changes in ex 4

--- WEEK4/script4.js
+++ WEEK4/script4.js
@@ -1,6 +1,10 @@
 function part1 () {
 var GOAT=document.getElementById('CHIKA');
 GOAT.firstChild.nodeValue ='STEPHEN000';
+
+var HOUSE=document.getElementById('LONDON');
+HOUSE.firstChild.nodeValue ='TRIDENT';
+
 }
 
 document.addEventListener('DOMContentLoaded', part1);
